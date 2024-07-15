c4f4242a36a4edc22440e6fe833331d9bb1c7a1e
LeiBuDao <leibudao@gmail.com>
2024-07-15 20:34:18 +0800

    :construction: 使用雪花算法生成key

--- member/src/main/java/com/leibudao/train/member/service/MemberService.java
+++ member/src/main/java/com/leibudao/train/member/service/MemberService.java
@@ -1,12 +1,20 @@
 package com.leibudao.train.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.ObjectUtil;
+import com.leibudao.train.common.exception.BusinessException;
+import com.leibudao.train.common.exception.BusinessExceptionEnum;
+import com.leibudao.train.common.util.SnowUtil;
 import com.leibudao.train.member.domain.Member;
+import com.leibudao.train.member.domain.MemberExample;
 import com.leibudao.train.member.mapper.MemberMapper;
 import com.leibudao.train.member.req.MemberRegisterReq;
 import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -16,12 +24,34 @@ public class MemberService {
     @Resource
     private MemberMapper memberMapper;
 
+    private Member selectByMobile(String mobile) {
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> list = memberMapper.selectByExample(memberExample);
+        System.out.println(mobile);
+        System.out.println(list);
+        if (CollUtil.isEmpty(list)) {
+            return null;
+        } else {
+            return list.get(0);
+        }
+    }
+
     public long register(MemberRegisterReq req) {
         String mobile = req.getMobile();
+        Member memberDB = selectByMobile(mobile);
+
+        if (!ObjectUtil.isNull(memberDB)) {
+            // return list.get(0).getId();
+            throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_EXIST);
+        }
+
         Member member = new Member();
-        member.setId(System.currentTimeMillis());
+        member.setId(SnowUtil.getSnowflakeNextId());
         member.setMobile(mobile);
         memberMapper.insert(member);
         return member.getId();
     }
+
+
 }
