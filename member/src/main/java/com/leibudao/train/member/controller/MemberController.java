65de06197029880cae6cd4ce4bc2dffcbbde22cc
LeiBuDao <leibudao@gmail.com>
2024-07-15 20:23:45 +0800

    :construction: 增加异常处理

--- member/src/main/java/com/leibudao/train/member/controller/MemberController.java
+++ member/src/main/java/com/leibudao/train/member/controller/MemberController.java
@@ -5,6 +5,7 @@ import com.leibudao.train.common.resp.CommonResp;
 import com.leibudao.train.member.req.MemberRegisterReq;
 import com.leibudao.train.member.service.MemberService;
 import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +18,9 @@ public class MemberController {
     private MemberService memberService;
 
     @PostMapping("/register")
-    public CommonResp<Long> register(MemberRegisterReq req) {
-        CommonResp<Long> commonResp = new CommonResp<>();
+    public CommonResp<Long> register(@Valid MemberRegisterReq req) {
         long id = memberService.register(req);
-        commonResp.setContent(id);
-        return commonResp;
+        return new CommonResp<>(id);
     }
 
 }
