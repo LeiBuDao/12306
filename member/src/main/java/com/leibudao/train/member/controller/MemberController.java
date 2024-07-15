ebec60a8d716559fabd12746c50b52784c4b9530
LeiBuDao <leibudao@gmail.com>
2024-07-15 19:56:54 +0800

    :construction: 封装请求参数和返回结果

--- member/src/main/java/com/leibudao/train/member/controller/MemberController.java
+++ member/src/main/java/com/leibudao/train/member/controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.leibudao.train.member.controller;
 
 
+import com.leibudao.train.common.resp.CommonResp;
+import com.leibudao.train.member.req.MemberRegisterReq;
 import com.leibudao.train.member.service.MemberService;
 import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +15,13 @@ public class MemberController {
 
     @Resource
     private MemberService memberService;
+
+    @PostMapping("/register")
+    public CommonResp<Long> register(MemberRegisterReq req) {
+        CommonResp<Long> commonResp = new CommonResp<>();
+        long id = memberService.register(req);
+        commonResp.setContent(id);
+        return commonResp;
+    }
 
 }
--- member/src/main/java/com/leibudao/train/member/service/MemberService.java
+++ member/src/main/java/com/leibudao/train/member/service/MemberService.java
@@ -1,6 +1,8 @@
 package com.leibudao.train.member.service;
 
+import com.leibudao.train.member.domain.Member;
 import com.leibudao.train.member.mapper.MemberMapper;
+import com.leibudao.train.member.req.MemberRegisterReq;
 import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,7 +16,12 @@ public class MemberService {
     @Resource
     private MemberMapper memberMapper;
 
-
-
-
+    public long register(MemberRegisterReq req) {
+        String mobile = req.getMobile();
+        Member member = new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+        memberMapper.insert(member);
+        return member.getId();
+    }
 }
