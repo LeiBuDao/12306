6cf2159f47c7663fc6f30b54099dd8c09131dae1
LeiBuDao <leibudao@gmail.com>
2024-07-16 15:03:06 +0800

    :sparkles: JWT单点登录功能实现

--- member/src/main/java/com/leibudao/train/member/controller/MemberController.java
+++ member/src/main/java/com/leibudao/train/member/controller/MemberController.java
@@ -2,11 +2,15 @@ package com.leibudao.train.member.controller;
 
 
 import com.leibudao.train.common.resp.CommonResp;
+import com.leibudao.train.member.req.MemberLoginReq;
 import com.leibudao.train.member.req.MemberRegisterReq;
+import com.leibudao.train.member.req.MemberSendCodeReq;
+import com.leibudao.train.member.resp.MemberLoginResp;
 import com.leibudao.train.member.service.MemberService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +26,19 @@ public class MemberController {
         long id = memberService.register(req);
         return new CommonResp<>(id);
     }
+
+    @PostMapping("/send-code")
+    public CommonResp<Long> sendCode(@Valid @RequestBody MemberSendCodeReq req) {
+        memberService.sendCode(req);
+        return new CommonResp<>();
+    }
+
+    @PostMapping("/login")
+    public CommonResp<MemberLoginResp> login(@Valid MemberLoginReq req) {
+        MemberLoginResp resp = memberService.login(req);
+        return new CommonResp<>(resp);
+    }
+
+
 
 }
